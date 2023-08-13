e6ce4937ff6ed64cce31ade0b0c59102411df573
SamuraX <matheusomr1@gmail.com>
2023-08-13 17:51:46 -0300

    feat(frontend): add home page styles

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './routes/home/home.component';
 import { TracksComponent } from './routes/tracks/tracks.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
+import { FooterComponent } from './routes/home/footer/footer.component';
 import { TrackComponent } from './routes/tracks/track/track.component';
 
 @NgModule({
