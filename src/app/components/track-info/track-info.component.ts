c608d81fd5da0ecedfc60e156b8dbe483e865cf4
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-25 17:54:48 -0300

    Track ionfo navigation

--- src/app/components/track-info/track-info.component.ts
+++ src/app/components/track-info/track-info.component.ts
@@ -1,21 +1,34 @@
 import { DatePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft, faQrcode } from '@fortawesome/free-solid-svg-icons';
 import { Track } from 'spotify-types';
 import formatTracks from '../../helpers/format-track.helper';
+import { TrackCardComponent } from '../track-card/track-card.component';
 
 @Component({
   selector: 'app-track-info',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, FontAwesomeModule, TrackCardComponent],
   templateUrl: './track-info.component.html',
   styleUrl: './track-info.component.scss',
 })
 export class TrackInfoComponent {
   private route = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
   private routeTracks = this.route.getCurrentNavigation()?.extras.state?.[
     'tracks'
   ] as Track[];
 
   protected tracks = formatTracks(this.routeTracks);
+  protected isrc = this.activatedRoute.snapshot.params['isrc'];
+  protected icons = {
+    faQrcode,
+    faChevronLeft,
+  };
+
+  protected back(): void {
+    this.route.navigate(['/home']);
+  }
 }
