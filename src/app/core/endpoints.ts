58dca96379aa25cd6023e68d35cbfcdc56ace846
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-25 12:54:03 -0300

    Help files

---
+++ src/app/core/endpoints.ts
@@ -0,0 +1,4 @@
+export const endpoints = {
+  accessToken: 'https://accounts.spotify.com/api/token',
+  search: 'https://api.spotify.com/v1/search',
+};
---
+++ src/app/helpers/format-track.helper.ts
@@ -0,0 +1,16 @@
+import { Track } from 'spotify-types';
+import TrackInfo from '../components/track-info/track-info.interface';
+
+const formatTracks = (tracks: Track[]): TrackInfo[] =>
+  tracks.map((track) => ({
+    name: track.name,
+    artists: track.artists.map((artist) => artist.name).join(', '),
+    availableInBR: !!track.available_markets?.includes('BR'),
+    previewUrl: track.preview_url,
+    albumThumbnailUrl: track.album.images[0].url,
+    duration: track.duration_ms,
+    releaseDate: track.album.release_date,
+    spotifyUrl: track.external_urls.spotify,
+  }));
+
+export default formatTracks;
