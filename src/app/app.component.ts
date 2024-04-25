b9f51efb9ebc5a54c3b395212032dc29d82d0c4f
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-25 12:56:34 -0300

    Refactor AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TokenService } from './core/token/token.service';
+import { SpotifyService } from './services/spotify.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
-  title = 'front-end-challenge';
+  private spotifyService = inject(SpotifyService);
+  private tokenService = inject(TokenService);
+
+  constructor() {
+    this.spotifyService.generateAccessToken().subscribe((res) => {
+      this.tokenService.setToken(res);
+    });
+  }
 }
