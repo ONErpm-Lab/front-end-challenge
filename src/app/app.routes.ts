7c3aa62037854b11a63962564e314cbafdf728a9
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-25 12:57:16 -0300

    Create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/home/home.component').then((c) => c.HomeComponent),
+  },
+  {
+    path: 'track-info/:isrc',
+    loadComponent: () =>
+      import('./components/track-info/track-info.component').then(
+        (c) => c.TrackInfoComponent
+      ),
+  },
+];
