e868064f3d620c4fd210bc72e2abac3803b76b84
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-25 12:57:05 -0300

    Add locale configs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
+import { registerLocaleData } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import portuguese from '@angular/common/locales/pt';
+
+registerLocaleData(portuguese, 'pt-BR');
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
