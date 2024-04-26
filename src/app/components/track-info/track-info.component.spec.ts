2251faee2fdbff282c180f42edaeca6db889db7d
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-26 15:51:59 -0300

    Creates test for TrackInfo component

--- src/app/components/track-info/track-info.component.spec.ts
+++ src/app/components/track-info/track-info.component.spec.ts
@@ -3,10 +3,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { registerLocaleData } from '@angular/common';
 import portuguese from '@angular/common/locales/pt';
 import { ActivatedRoute, Router } from '@angular/router';
-import { spotifyTracksMockArray } from '../../tests/mocks/track-mock';
+import {
+  spotifyTracksMockArray,
+  trackInfoMockArray,
+} from '../../tests/mocks/track-mock';
 import { TrackInfoComponent } from './track-info.component';
 
 registerLocaleData(portuguese, 'pt-BR');
+
+const isrcMock = 'BRC310600002';
 
 describe('TrackInfoComponent', () => {
   let component: TrackInfoComponent;
@@ -19,23 +24,16 @@ describe('TrackInfoComponent', () => {
         {
           provide: ActivatedRoute,
           useValue: {
-            snapshot: { params: { isrc: () => 'BRC310600002' } },
-          },
-        },
-        {
-          provide: Router,
-          useValue: {
-            getCurrentNavigation: () => ({
-              extras: {
-                state: {
-                  tracks: spotifyTracksMockArray,
-                },
-              },
-            }),
+            snapshot: { params: { isrc: isrcMock } },
           },
         },
       ],
     }).compileComponents();
+
+    const router = TestBed.inject(Router);
+    spyOn(router, 'getCurrentNavigation').and.returnValue({
+      extras: { state: { tracks: spotifyTracksMockArray } },
+    } as any);
 
     fixture = TestBed.createComponent(TrackInfoComponent);
     component = fixture.componentInstance;
@@ -45,5 +43,24 @@ describe('TrackInfoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should transform the router received Spotify Tracks into TrackInfo array', () => {
+    const tracks = (component as any)['tracks'];
+
+    expect(tracks).toEqual(trackInfoMockArray);
+  });
+
+  it('should get the isrc from the activated route params', () => {
+    const isrc = (component as any)['isrc'];
+    expect(isrc).toEqual(isrcMock);
+  });
+
+  it('#back should navigate to /home', () => {
+    const spy = spyOn(component['route'], 'navigate');
+
+    component['back']();
+
+    expect(spy).toHaveBeenCalledWith(['/home']);
   });
 });
