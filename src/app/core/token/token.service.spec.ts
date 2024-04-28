5f613c27bfad61ef037cb83c9568489b9136efcd
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-04-28 15:11:06 -0300

    Create tests for token service

--- src/app/core/token/token.service.spec.ts
+++ src/app/core/token/token.service.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
+import { AccessToken } from 'spotify-types';
+import { accessTokenMock } from '../../tests/mocks/access-token.mock';
 import { TokenService } from './token.service';
 
 describe('TokenService', () => {
@@ -12,5 +14,42 @@ describe('TokenService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return null when no token is set', () => {
+    expect(service.getToken()).toBeNull();
+  });
+
+  it('should set and return token', () => {
+    service.setToken(accessTokenMock);
+    expect(service.getToken()).toEqual(accessTokenMock);
+  });
+
+  it('should remove token after expiration', (done) => {
+    const token: AccessToken = { ...accessTokenMock, expires_in: 0.2 };
+
+    service.setToken(token);
+
+    setTimeout(() => {
+      expect(service.getToken()).toBeNull();
+      done();
+    }, 200);
+  });
+
+  it('should update token', () => {
+    const token1: AccessToken = {
+      ...accessTokenMock,
+      access_token: 'token1',
+    };
+
+    const token2: AccessToken = {
+      ...accessTokenMock,
+      access_token: 'token2',
+    };
+
+    service.setToken(token1);
+    service.setToken(token2);
+
+    expect(service.getToken()).toEqual(token2);
   });
 });
