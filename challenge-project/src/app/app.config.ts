cd1b8a6248d11a693dd8f478f4f3144f0e46f5e5
Amanda de Souza Medeiros <medeiros.amandas@gmail.com>
2024-08-24 14:06:42 -0300

    config environment

--- challenge-project/src/app/app.config.ts
+++ challenge-project/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -7,5 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
   ],
 };
---
+++ challenge-project/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  produtction: false,
+  spotifyClientId: '6454e80fbd034d919d545b5972eb1c31',
+  spotifyClientSecret: 'd1d8fcdb980b43d88d067ba92b9bd4b6',
+  redirectUri: 'http.localhost:4200/callback',
+};
