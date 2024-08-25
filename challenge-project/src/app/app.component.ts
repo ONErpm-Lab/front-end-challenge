da8ce738f711dac3ad0b10ada7f23b28c57b3e2a
Amanda de Souza Medeiros <medeiros.amandas@gmail.com>
2024-08-25 19:57:20 -0300

    criando component card

--- challenge-project/src/app/app.component.ts
+++ challenge-project/src/app/app.component.ts
@@ -1,63 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SpotifyService } from './services/spotify.service';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'challenge-project';
-
-  artistData: any;
-  trackData: any;
-  track: any[] = [];
-
-  isrcs: string[] = [
-    'US7VG1846811',
-    'US7QQ1846811',
-    'BRC310600002',
-    'BR1SP1200071',
-    'BR1SP1200070',
-    'BR1SP1500002',
-    'BXKZM1900338',
-    'BXKZM1900345',
-    'QZNJX2081700',
-    'QZNJX2078148',
-  ];
-
-  tracks: any[] = [];
-
-  constructor(private spotifyService: SpotifyService) {}
-  ngOnInit(): void {
-    /*
-    this.spotifyService.getArtistData('4Z8W4fKeB5YxbusRsdQVPb').subscribe({
-      next: (data) => {
-        this.artistData = data;
-        console.log('Artist data:', this.artistData);
-      },
-      error: (error) => {
-        console.error('API connection failed', error);
-      },
-    });
-  */
-
-    this.isrcs.forEach((isrc) => {
-      this.spotifyService.getTrackData(isrc).subscribe({
-        next: (track) => {
-          this.tracks.push({ isrc, ...track.tracks.items[0] });
-        },
-        error: (error) => {
-          console.log('ERRO');
-          console.log(error);
-        },
-      });
-    });
-
-    console.log(this.tracks);
-  }
 }
---
+++ challenge-project/src/app/components/card/card.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent implements OnInit {
+  artistData: any;
+  trackData: any;
+  track: any[] = [];
+
+  isrcs: string[] = [
+    'US7VG1846811',
+    'US7QQ1846811',
+    'BRC310600002',
+    'BR1SP1200071',
+    'BR1SP1200070',
+    'BR1SP1500002',
+    'BXKZM1900338',
+    'BXKZM1900345',
+    'QZNJX2081700',
+    'QZNJX2078148',
+  ];
+
+  tracks: any[] = [];
+
+  constructor(private spotifyService: SpotifyService) {}
+  ngOnInit(): void {
+    /*
+    this.spotifyService.getArtistData('4Z8W4fKeB5YxbusRsdQVPb').subscribe({
+      next: (data) => {
+        this.artistData = data;
+        console.log('Artist data:', this.artistData);
+      },
+      error: (error) => {
+        console.error('API connection failed', error);
+      },
+    });
+  */
+
+    this.isrcs.forEach((isrc) => {
+      this.spotifyService.getTrackData(isrc).subscribe({
+        next: (track) => {
+          this.tracks.push({ isrc, ...track.tracks.items[0] });
+        },
+        error: (error) => {
+          console.log('ERRO');
+          console.log(error);
+        },
+      });
+    });
+
+    console.log(this.tracks);
+  }
+}
