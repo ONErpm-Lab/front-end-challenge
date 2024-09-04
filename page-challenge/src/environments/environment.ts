140abff62bd7d76f7b5fdf9b818cbc5134f6a3b0
Cicera Ribeiro <maccicera@gmail.com>
2024-09-04 14:23:06 -0300

    fix: token

--- page-challenge/src/environments/environment.ts
+++ page-challenge/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     spotify: {
         baseUrl: 'https://api.spotify.com/v1',
     },
-    accessToken: 'BQCkiye1LAHnX12GK_tWj7wKdd2TRCJb3Moy0pR8QiDnaBekvGcmtYRmn_BxxvURHvhrvkOspWfDHNc8k1kMW0HRUqQN4dY__Y5EYf2rO32lnbnC81FvTqhCjTDRXpQ6gmPIz10UMPLfgmcFt65WlqtiAoDdrhfC4R4g9UhpwzTBxGhNHxeo-FKTXK6ViWkgBF695LDjhaAtVhv2xWv0jtgrwMR49A'
+    accessToken: 'YOUR_ACCESS_TOKEN_HERE'
 }
