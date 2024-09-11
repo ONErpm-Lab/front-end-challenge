a4972a21e99b429a5a6376011dc4b209df0b38dd
gabrielliosc <gabrielli.osc@gmail.com>
2024-09-11 20:38:33 -0300

    feat(app): Create SongInterface

---
+++ songs-webapp/src/app/SongInterface.ts
@@ -0,0 +1,11 @@
+export interface SongInterface {
+    id: number,
+    title: string,
+    artist: string,
+    album: string,
+    date: string,
+    duration: string,
+    spotifyLink: string,
+    avaiableBr: boolean,
+    image: string
+}
