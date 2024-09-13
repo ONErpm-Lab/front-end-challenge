94f2a6fa56e1ef72773bbcd2aca2db9bc7465488
gabrielliosc <gabrielli.osc@gmail.com>
2024-09-12 23:51:11 -0300

    chore: Add .env config file

---
+++ songs-webapp/src/env.d.ts
@@ -0,0 +1,34 @@
+// Define the type of the environment variables.
+declare interface Env {
+  readonly NODE_ENV: string;
+  // Replace the following with your own environment variables.
+  readonly NG_APP_SPOTIFY_API_URL: string;
+
+  readonly NG_APP_SPOTIFY_CLIENT_ID: string;
+
+  readonly NG_APP_SPOTIFY_CLIENT_SECRET: string;
+
+  readonly NG_APP_TOKEN_API_URL: string;
+
+  [key: string]: any;
+}
+
+// Choose how to access the environment variables.
+// Remove the unused options.
+
+// 1. Use import.meta.env.YOUR_ENV_VAR in your code. (conventional)
+declare interface ImportMeta {
+  readonly env: Env;
+}
+
+// 2. Use _NGX_ENV_.YOUR_ENV_VAR in your code. (customizable)
+// You can modify the name of the variable in angular.json.
+// ngxEnv: {
+//  define: '_NGX_ENV_',
+// }
+declare const _NGX_ENV_: Env;
+
+// 3. Use process.env.YOUR_ENV_VAR in your code. (deprecated)
+declare namespace NodeJS {
+  export interface ProcessEnv extends Env {}
+}
