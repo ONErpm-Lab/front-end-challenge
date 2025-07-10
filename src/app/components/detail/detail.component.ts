b0011adc29c9587b82e4a526b6238b514c4a0aa5
viniciusamicci <vini_amicci@hotmail.com>
2025-07-10 18:31:18 -0300

    create components list and detail

---
+++ src/app/components/detail/detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  imports: [],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.scss'
+})
+export class DetailComponent {
+
+}
---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  imports: [],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
+})
+export class ListComponent {
+
+}
