31d6a0a68640b3ed220d9235e4b877f2635bdcbe
viniciusamicci <vini_amicci@hotmail.com>
2025-07-10 19:24:46 -0300

    create list component

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,11 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TokenService } from './../../services/token/token.service';
+
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-list',
-  imports: [],
+    imports: [MatButtonModule, MatCardModule, MatPaginatorModule, MatListModule, MatButtonModule, CommonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  urlImage = `${environment.urlImage}`
+  movies = [
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  },
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  },
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  },
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  },
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  },
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  },
+  {
+    poster_path: "movie.poster_path",
+    title: "movie.title",
+    release_date: new Date(),
+    id: "movie.id"
+  }
+]
+  paginateData: any[] = [];
+  totalItems = this.movies.length;
+  pageSize = 3;
+  pageIndex = 0;
+ 
+
+  constructor(
+    private router: Router,
+    private tokenService: TokenService
+  ) {}
+
+  ngOnInit(): void {
+    this.tokenService.getAccessToken().subscribe({
+      next: (res) => {
+        this.tokenService.saveToken(res);
+      },
+      error: (err) => {
+        console.log('>>>>>>>>>>> ERROR', err);
+      }
+    })
+    this.updatePaginatedData();
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.updatePaginatedData();
+  }
+
+  updatePaginatedData(): void {
+    const start = this.pageIndex * this.pageSize;
+    const end = start + this.pageSize;
+    this.paginateData = this.movies.slice(start, end);
+  }
+
+    navigateDetails(id: number): void {
+      this.router.navigate(['/details', id]);
+    }
+
+
 
 }
