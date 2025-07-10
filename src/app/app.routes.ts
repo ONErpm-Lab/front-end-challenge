336ee12a6de9728624bfeb005a88a9ebf8736d5a
viniciusamicci <vini_amicci@hotmail.com>
2025-07-10 19:23:30 -0300

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list',
+    loadComponent: () =>
+      import('./components/list/list.component').then((l) => l.ListComponent),
+  },
+  {
+    path: 'detail',
+    loadComponent: () =>
+      import('./components/detail/detail.component').then((d) => d.DetailComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
