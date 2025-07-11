1c42affb7a037fa662a2a432ed3bf55705e20843
Marcos <marcosvieirar@outlook.com>
2025-07-11 18:06:38 -0300

    feat: add production and development environment configurations for Spotify API

---
+++ spotify-onerpm/src/environments/environments.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  spotify: {
+    clientId: "e1c20ab2213e4b5fa0031ba6e4b1c6d6",
+    clientSecret: "7ea8cab987ca4b339e3f507042941d80",
+    tokenUrl: "https://accounts.spotify.com/api/token",
+    searchUrl: "https://api.spotify.com/v1/search"
+  }
+};
