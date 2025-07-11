cfe1d7bfbf493aec4e83f66f308072ed77c370c2
viniciusamicci <vini_amicci@hotmail.com>
2025-07-10 23:27:45 -0300

    config interceptor

--- src/app/core/interceptors/interceptor.service.ts
+++ src/app/core/interceptors/interceptor.service.ts
@@ -5,17 +5,17 @@ import { environment } from '../../../environments/environment';
 import { AuthService } from '../auth/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> => {
-  console.log('>>>>>>>>>>> session', );
   const authService = inject(AuthService);
-  const token = authService.getToken('token');
+  const sessionToken = authService.getToken('token');
 
-  if (req.url === `${environment.apiUrl}/token`) {
+  if (req.url === `${environment.accountsUrl}/token`) {
     return next(req);
   }
 
-  if (token) {
+  if (sessionToken) {
+    const token: any = JSON.parse(sessionToken);
     const clonedRequest = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
+      headers: req.headers.set('Authorization', `Bearer ${token.access_token}`)
     });
     return next(clonedRequest);
   }
