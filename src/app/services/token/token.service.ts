0ee47bce187faa398b234e6bbab47de928331f4a
viniciusamicci <vini_amicci@hotmail.com>
2025-07-10 23:29:02 -0300

    config token service

--- src/app/services/token/token.service.ts
+++ src/app/services/token/token.service.ts
@@ -7,9 +7,9 @@ import { environment } from "../../../environments/environment";
   providedIn: "root",
 })
 export class TokenService {
-private tokenUrl = `${environment.apiUrl}/token`;
-  private clientId = '57c21d30e79c44d39415a4fc0a157afa'; // Substitua pelo seu Client ID
-  private clientSecret = 'eb21c7b523af4c47bfa94835e84527c5'; // Substitua pelo seu Client Secret
+private tokenUrl = `${environment.accountsUrl}/token`;
+  private clientId = ''; // Replace with yours Client ID
+  private clientSecret = ''; // Replace with yours Client Secret
 
   constructor(private http: HttpClient) {}
 
