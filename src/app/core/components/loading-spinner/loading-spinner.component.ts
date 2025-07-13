50db5905d31ebe69d200eafe6ccff90a54c6f5ff
cnx-deividyhertz <deividy.hertz@conexasaude.com.br>
2025-07-13 13:41:32 -0300

    feat: new component loading spinner

---
+++ src/app/core/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.scss']
+})
+export class LoadingSpinnerComponent {}
