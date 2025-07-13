c828b91e479ae79b6946cac991456ca805b8dfcc
cnx-deividyhertz <deividy.hertz@conexasaude.com.br>
2025-07-13 13:40:31 -0300

    feat: new component header

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+  constructor() {}
+}
