aa87e03ab081f6dfa42caadd2e7a0beca7a0b1ba
cnx-deividyhertz <deividy.hertz@conexasaude.com.br>
2025-07-13 13:46:58 -0300

    feat: duration format util

---
+++ src/app/core/utils/duration.ts
@@ -0,0 +1,6 @@
+export const formatDuration = (duration: number) => {
+    const minutes = Math.floor(duration / 60000);
+    const seconds = Math.floor((duration % 60000) / 1000);
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+};
+
