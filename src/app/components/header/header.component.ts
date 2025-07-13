200d521c967b19dc5fa03cccb360b208ad18e4dc
viniciusamicci <vini_amicci@hotmail.com>
2025-07-13 19:01:33 -0300

    add toogle pagination

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  checked = false;
+  disabled = false;
 
 }
