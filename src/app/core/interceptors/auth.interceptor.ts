3f0a43d326423afcb9f8aa2e1283b5b533d5bf57
Your NameRafael <rafaaquino28@gmail.com>
2025-07-21 17:29:40 -0300

    feat: interceptor send token

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.dev';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = environment.token;
+    let authReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return next.handle(authReq);
+  }
+}
