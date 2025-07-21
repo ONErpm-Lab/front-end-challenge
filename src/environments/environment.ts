c157682d1ef435f1c820216ac1d4b6de43d90fe3
Your NameRafael <rafaaquino28@gmail.com>
2025-07-21 17:29:27 -0300

    feat: environment project

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  host_api: 'https://herokuapp.com',
+  token:
+    'BQDut8dGEZ46Ssga9Mp2YkWJgA5RAOzo5gjUpSa7mAKFc-RpjmjF9puuJqnb8JtmNzce5IcSXDpZ5VLcxCoLN_YYW3MD-I0xmeAshTt7wKydYs9bV_S-Dg9CMkkW7rcLqyMRKnGvIFaIdVJtP_401PuCD2CGZeoNki_PSP6-KHMYpEHel1bGqWqnERwK1Exs10fa0zZtujO8vTquB-OG_qH9WRca1CSTL363Q4NWQ7KeM5A1WAZ-Stm_HV5pZmb4nw3IuC_8boBF5NtczXYgg8237Qn-K2IBP78dqkIMftlgq28uwZBJM-kT4fcQYg61',
+};
