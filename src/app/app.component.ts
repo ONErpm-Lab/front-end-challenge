b3038d5d2577f4602ff65fc56e3f79eb1a6c7f9c
Your NameRafael <rafaaquino28@gmail.com>
2025-07-23 01:16:39 -0300

    feat: app componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayerComponent } from './features/components/player/player.component';
+import { CommonModule } from '@angular/common';
+import { SearchComponent } from './features/components/search/search.component';
+import { TrackComponent } from './features/components/track/track.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PlayerComponent,
+    SearchComponent,
+    TrackComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'challenger-pyou';
+  tracks = [];
 }
