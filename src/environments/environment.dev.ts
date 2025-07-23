319138b6b10cf80e09c7775bc8aad85d4b501eda
Your NameRafael <rafaaquino28@gmail.com>
2025-07-23 14:33:21 -0300

    feat: env dev

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  host_api: 'http://localhost:4200',
+  searchUrl: 'https://api.spotify.com/v1/search',
+  tokenUrl: 'https://accounts.spotify.com/api/token',
+  client_id: '06dd1d8dcc67417784758dc5e0e85f2f',
+  client_secret: 'e3e02e69f146496690427af5a82216fb',
+};
